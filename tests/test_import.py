b9c1171d275103c8e87dc05ac6b2deca20141e72
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-03-08 15:54:55 -0500

    Test basic ability to import and get the version

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present Mike Boyle <michael.oliver.boyle@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+def test_import():
+    import sxscatalog
+    assert sxscatalog.__version__
