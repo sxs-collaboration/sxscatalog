766f4759188c7dab9d2c23107f45c718bbd5ca16
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-04-01 11:02:11 -0400

    Test loading simulations and dataframe

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,3 +5,19 @@
 def test_import():
     import sxscatalog
     assert sxscatalog.__version__
+
+def test_load_simulations():
+    import sxscatalog
+    sxscatalog.load("simulations")
+
+def test_load_simulations_tagged():
+    import sxscatalog
+    sxscatalog.load("simulations", tag="3.0.0a4")
+
+def test_load_dataframe():
+    import sxscatalog
+    sxscatalog.load("dataframe")
+
+def test_load_dataframe_tagged():
+    import sxscatalog
+    sxscatalog.load("dataframe", tag="3.0.0a4")
