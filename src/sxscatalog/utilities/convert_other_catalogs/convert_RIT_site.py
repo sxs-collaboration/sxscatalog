51ec622fd22c29e3019a8b898aa1b4ce7154e6a2
Leo C. Stein <leo.stein@gmail.com>
2026-02-05 16:27:24 -0600

    Use consolidate_xyz_vectors for convert_RIT_site.
    Also format with black, and use argparse.FileType.

--- src/sxscatalog/utilities/convert_other_catalogs/convert_RIT_site.py
+++ src/sxscatalog/utilities/convert_other_catalogs/convert_RIT_site.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python3
 
-def fetch_RIT_catalog_data():
-    """ 
-    Fetch metadata from the RIT Catalog to json file.
 
-    This function scrapes the RIT Catalog page 
-    and extract information about the numerical 
-    simulations. The RIT Catalog can be accessed 
-    through
-    https://ccrg.rit.edu/numerical-simulations, 
-    but the actual data is hosted at 
-    https://ccrgpages.rit.edu/~RITCatalog/. The 
-    function retrieves the simulation IDs, download 
-    links for extrapolated Psi4 and Strain data, and 
-    the metadata for each simulation. The metadata is
-    parsed and stored in a structured format. When the
-    script is run directly, it saves the data into a 
-    JSON file specified by the user. The default name 
-    for the output file is "RIT_metadata.json".
+def fetch_RIT_catalog_data():
+    """Fetch metadata from the RIT Catalog and parse as a dict.
+
+    Scrape the RIT Catalog page and extract information about the numerical
+    simulations. The RIT Catalog can be accessed through
+    https://ccrg.rit.edu/numerical-simulations, but the actual data is hosted at
+    https://ccrgpages.rit.edu/~RITCatalog/. Retrieve the simulation IDs,
+    download links for extrapolated Psi4 and Strain data, and the metadata for
+    each simulation. The metadata is parsed into a dict.
 
     Returns
-    -------
-    json file
-        This json file contains a list of all the
-        simulations, with their respective download
-        links for Psi4, strain, and metadata and the 
-        metadata itself.
+    =======
+    dict
+        A dictionary mapping simulation name to metadata.
+
     """
-    from sxscatalog.metadata.metadata import Metadata
+
+    from ...metadata.metadata import Metadata
+
     import requests
     import tempfile
     from bs4 import BeautifulSoup
 
-    
     RITcatalog_url = "https://ccrgpages.rit.edu/~RITCatalog/"
 
     session = requests.Session()
@@ -65,12 +56,15 @@ def fetch_RIT_catalog_data():
         links = cells[2].find_all("a", href=True)
 
         if len(links) != 3:
-            raise Exception(f"This function was expecting 3 links, but it is getting an unexpected number of links for simulation {sim_id}.")
+            raise Exception(
+                "Expecting 3 links, but getting an unexpected number of links "
+                f"for simulation {sim_id}."
+            )
 
         # Validate links contain expected keywords
         psi_link = strain_link = metadata_link = None
         for link in links:
-            href = link['href'].lower()
+            href = link["href"].lower()
             if "psi" in href:
                 psi_link = link
             elif "strain" in href:
@@ -84,7 +78,7 @@ def fetch_RIT_catalog_data():
         RITcatalogdata[sim_id] = {}
 
         # Fetching metadata file
-        metadata_response = session.get(RITcatalog_url + metadata_link['href'])
+        metadata_response = session.get(RITcatalog_url + metadata_link["href"])
         if metadata_response.status_code != 200:
             raise Exception(f"Failed to load page for simulation {str(sim_id)}")
 
@@ -97,28 +91,55 @@ def fetch_RIT_catalog_data():
             tmp_path = tmp.name
             tmp.seek(0)
             RITcatalogdata[sim_id] = Metadata.from_txt_file(tmp_path, cache_json=False)
+
+            # from_txt_file() adds metadata_path, which is meaningless here.
+            # _backwards_compatibility() adds number_of_orbits=NaN, which we
+            # don't want. Throw away both of these keys.
             RITcatalogdata[sim_id].pop("metadata_path", None)
             RITcatalogdata[sim_id].pop("number_of_orbits", None)
 
-        RITcatalogdata[sim_id]["extrap_psi4_url"] = RITcatalog_url + psi_link['href']
-        RITcatalogdata[sim_id]["extrap_strain_url"] = RITcatalog_url + strain_link['href']
-        RITcatalogdata[sim_id]["metadata_url"] = RITcatalog_url + metadata_link['href']
+        RITcatalogdata[sim_id]["extrap_psi4_url"] = RITcatalog_url + psi_link["href"]
+        RITcatalogdata[sim_id]["extrap_strain_url"] = (
+            RITcatalog_url + strain_link["href"]
+        )
+        RITcatalogdata[sim_id]["metadata_url"] = RITcatalog_url + metadata_link["href"]
 
     return RITcatalogdata
+
 
 if __name__ == "__main__":
 
     import json
     import argparse
 
-    parser = argparse.ArgumentParser(description="Fetch RIT Catalog data and save to JSON.")
+    # absolute imports in a standalone script
+    from sxscatalog.utilities.convert_other_catalogs.convert_RIT_site import (
+        fetch_RIT_catalog_data,
+    )
+    from sxscatalog.utilities import consolidate_xyz_vectors
 
-    parser.add_argument("--output", type=str, default="RIT_metadata.json", help="Output JSON file name.")
+    parser = argparse.ArgumentParser(
+        description="Fetch RIT Catalog data and save to JSON."
+    )
+
+    parser.add_argument(
+        "--output",
+        type=argparse.FileType("w"),
+        default="RIT_catalog.json",
+        help="Output JSON file name (default: %(default)s).",
+    )
+    parser.add_argument(
+        "--consolidate-xyz-vectors",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="if triples of <key>x <key>y <key>z should be consolidated "
+        "into vectors (default: %(default)s)",
+    )
 
     args = parser.parse_args()
 
-    data = fetch_RIT_catalog_data()
+    data_dict = fetch_RIT_catalog_data()
+    if args.consolidate_xyz_vectors:
+        data_dict = {k: consolidate_xyz_vectors(v) for k, v in data_dict.items()}
 
-    # Writing the data to a JSON file
-    with open(args.output, "w") as f:
-        json.dump(data, f, indent=2, sort_keys=True)
+    json.dump(data_dict, args.output, indent=2, sort_keys=True)
