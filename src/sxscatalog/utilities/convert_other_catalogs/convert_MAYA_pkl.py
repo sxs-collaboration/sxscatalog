25eeaa7d686096a0e8091682b39054fe597cfc85
Leo C. Stein <leo.stein@gmail.com>
2026-02-05 15:41:57 -0600

    Add consolidate_xyz_vectors, better arg parsing for convert_MAYA_pkl

--- src/sxscatalog/utilities/convert_other_catalogs/convert_MAYA_pkl.py
+++ src/sxscatalog/utilities/convert_other_catalogs/convert_MAYA_pkl.py
@@ -1,58 +1,45 @@
 #!/usr/bin/env python
 
-
-def convert_maya_pkl_to_json(filename, output="MAYA_catalog.json"):
-    """Convert MAYA metadata pickle file to a json file.
-    Parameters
-    ==========
-    filename : str or Path
-        Relative or absolute path to the input pickle file.
-    output : str or Path
-        Path to the output json file.
-    """
-    import pandas as pd
-    from pathlib import Path
-    import json
-
-    # Make sure the file exists
-    path = Path(filename).expanduser().resolve()
-    if not path.exists():
-        raise FileNotFoundError(f"Could not find {path}")
-
-    data = pd.read_pickle(path)
-    data_dict = data.to_dict(orient="index")
-
-    vectors = {"a1": ["a1x", "a1y", "a1z"], "a2": ["a2x", "a2y", "a2z"]}
-
-    for values in data_dict.values():
-        for spin_label, components in vectors.items():
-            values[spin_label] = [values.pop(c) for c in components]
-
-    with open(output, "w") as f:
-        json.dump(data_dict, f)
-
-
 if __name__ == "__main__":
     import argparse
+    import json
+    import pandas as pd
+    from pathlib import Path
+
+    from sxscatalog.utilities import consolidate_xyz_vectors
 
     parser = argparse.ArgumentParser(
         description="Compute the MAYA_catalog.json file from MAYAmetadata.pkl",
         epilog=(
-            "This simply reads the pandas dataframe of the pickle file, and arranges it so that it is similar to SXS catalog.json."
+            "Read the pandas dataframe of the pickle file, and "
+            "arranges it so that it is similar to SXS catalog.json."
         ),
     )
     parser.add_argument(
         "filename",
+        type=argparse.FileType("rb"),
         help="path to the MAYAmetadata.pkl file",
     )
     parser.add_argument(
         "--output",
+        type=argparse.FileType("w"),
         default="MAYA_catalog.json",
-        help="path to the output file.",
+        help="path to the output file (default: %(default)s).",
     )
+    parser.add_argument(
+        "--consolidate-xyz-vectors",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="if triples of <key>x <key>y <key>z should be consolidated "
+        "into vectors (default: %(default)s)",
+    )
+
     args = parser.parse_args()
 
-    convert_maya_pkl_to_json(
-        args.filename,
-        output=args.output,
-    )
+    data = pd.read_pickle(args.filename)
+    data_dict = data.to_dict(orient="index")
+
+    if args.consolidate_xyz_vectors:
+        data_dict = {k: consolidate_xyz_vectors(v) for k, v in data_dict.items()}
+
+    json.dump(data_dict, args.output, indent=2, sort_keys=True)
